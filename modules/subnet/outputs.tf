de9384627f9716d74711157fb8b3e77fc1f559c4
Xiaochi-Li <seanli@geniesolutions.com.au>
2024-05-08 13:47:14 +1000

    add more outputs in all reusable modules.

--- modules/subnet/outputs.tf
+++ modules/subnet/outputs.tf
@@ -3,9 +3,29 @@ output "public_subnets" {
   value       = local.public_subnet
 }
 
+output "public_route_table_id" {
+  description = "value of public route table id"
+  value       = aws_route_table.public.id
+}
+
+output "nat_eip" {
+  description = "value of nat eip"
+  value       = aws_eip.nat_eip
+}
+
+output "nat_eip_id" {
+  description = "value of nat eip id"
+  value       = aws_eip.nat_eip.id
+}
+
 output "private_subnets" {
   description = "value of private subnets"
   value       = local.private_subnets
+}
+
+output "private_route_table_id" {
+  description = "value of private route table id"
+  value       = aws_route_table.private.id
 }
 
 output "nat_gateway_id" {
