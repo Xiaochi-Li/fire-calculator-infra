9f600f36fb5362fd115bbd25b7809656d129bcfa
Xiaochi-Li <seanli@geniesolutions.com.au>
2024-05-10 11:07:50 +1000

    chore: move backend configuration to its own file for Terraform Cloud integration

--- apps/development-envrionment/ap-southeast-2/main.tf
+++ apps/development-envrionment/ap-southeast-2/main.tf
@@ -1,19 +1,4 @@
-terraform {
-  cloud {
-    organization = "sean-li-terraform-cloud-learning"
 
-    workspaces {
-      name = "fire-calculator-infra-development"
-    }
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
 
 locals {
   envrionment      = "dev"
