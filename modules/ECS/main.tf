4ba74026ebf8c8d519162b45cfd186c32b815e82
Xiaochi-Li <xiaochi.li.webdev@gmail.com>
2024-06-19 10:32:08 +1000

    chore: Update main.tf to fix awslogs-create-group value in ECS task definition

--- modules/ECS/main.tf
+++ modules/ECS/main.tf
@@ -68,7 +68,7 @@ resource "aws_ecs_task_definition" "main" {
     logConfiguration = {
       logDriver = "awslogs"
       options = {
-        "awslogs-create-group"  = true
+        "awslogs-create-group"  = "true"
         "awslogs-group"         = aws_cloudwatch_log_group.main.name
         "awslogs-region"        = var.aws_region
         "awslogs-stream-prefix" = "${var.application_name}-${var.envrionment}"
